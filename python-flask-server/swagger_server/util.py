decf29b7e30ef787be0079f184b9aafaf0ce51db
Melvin <31448155+melvio@users.noreply.github.com>
2021-02-11 11:49:09 +0100

    updated the _deserialize method

--- python-flask-server/swagger_server/util.py
+++ python-flask-server/swagger_server/util.py
@@ -1,20 +1,16 @@
 import datetime
 
 import six
-import typing
 
 
 def _deserialize(data, klass):
     """Deserializes dict, list, str into an object.
-
     :param data: dict, list or str.
     :param klass: class literal, or string of class name.
-
     :return: object.
     """
     if data is None:
         return None
-
     if klass in six.integer_types or klass in (float, str, bool):
         return _deserialize_primitive(data, klass)
     elif klass == object:
@@ -23,13 +19,16 @@ def _deserialize(data, klass):
         return deserialize_date(data)
     elif klass == datetime.datetime:
         return deserialize_datetime(data)
-    elif type(klass) == typing.GenericMeta:
-        if klass.__extra__ == list:
-            return _deserialize_list(data, klass.__args__[0])
-        if klass.__extra__ == dict:
-            return _deserialize_dict(data, klass.__args__[1])
+    elif hasattr(klass, '__origin__'):
+
+    if klass.__origin__ == list:
+        return _deserialize_list(data, klass.__args__[0])
+    if klass.__origin__ == dict:
+        return _deserialize_dict(data, klass.__args__[1])
     else:
         return deserialize_model(data, klass)
+
+
 
 
 def _deserialize_primitive(data, klass):
